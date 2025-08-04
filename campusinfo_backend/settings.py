68ce7ecc5e28629119e54c7951406a39a50607d1
Temiloluwa Gboyega <temiloluwagboyega@gmail.com>
2025-08-04 18:08:12 +0100

    changes

--- campusinfo_backend/settings.py
+++ campusinfo_backend/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-cf!^-d3bga-+qt0gk(buq)0ej%$)nwgpe!vu&@(3az5p^w3y50
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['campusinfo_backend.onrender.com']
+ALLOWED_HOSTS = ['campus-info.onrender.com']
 
 
 
